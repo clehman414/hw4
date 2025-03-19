abbf9a4eca6ea4ab3c3ed3e630e13e8ca555cbc9
Caitlin Lehman <caitlin.lehman@kellogg.northwestern.edu>
2025-03-19 02:12:20 +0000

    Creates login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+  get("/login", { :controller => "sessions", :action => "new"})
+  post("/login", { :controller => "sessions", :action => "create"})
+  delete("/logout", { :controller => "sessions", :action => "destroy"})
+  
   resources "entries"
   resources "places"
   resources "sessions"
   resources "users"
+
+
+
 end
